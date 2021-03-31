6cd92fc6ebf2525389331955d1b2785972f35fd0
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-03-31 15:18:14 -0600

    Add variables assigned to integers or floats

--- model_1.rb
+++ model_1.rb
@@ -1,5 +1,11 @@
+#variables assigned to strings
 dog_name = "Gertie"
 dog_breed = "collie mix"
 dog_age = "3"
 dog_size = "medium"
 dog_temperment = "neutral"
+
+#variables assigned to intergers/floats
+number_of_dogs = 1
+mileage_walked_a_day = 2.5
+baths_a_year = 4
